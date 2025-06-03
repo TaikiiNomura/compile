9483d371624ae1c00bc061e8f17cfd3bcedd8271
TaikiiNomura <23b2119@sit.shonan-it.ac.jp>
2025-06-03 11:54:12 +0900

    Create Rev_sb.java

---
+++ project/Rev_sb.java
@@ -0,0 +1,8 @@
+public class Rev_sb
+{ 
+    public static void main(String[] args)
+    { 
+        StringBuffer sb = new StringBuffer("春夏秋冬"); 
+        System.out.println(sb.reverse()); 
+    } 
+}

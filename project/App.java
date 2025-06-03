1a52d19755b08c3b1083a4f170d74fb7e163c29c
TaikiiNomura <23b2119@sit.shonan-it.ac.jp>
2025-06-03 12:12:23 +0900

    Create App.java

---
+++ project/App.java
@@ -0,0 +1,10 @@
+public class App 
+{ 
+    public static void main(String[] args) 
+    { 
+        StringBuffer sb = new StringBuffer( 
+            "勝利も敗北も知り逃げ回って涙を流して男は一人前になる！\n" 
+            + "泣いたっていいんだ！" ); 
+        System.out.println(sb.append("乗り越えろ！"));
+    } 
+}

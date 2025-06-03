13b560dbad13c90f61aed7785a508cc16159b611
TaikiiNomura <23b2119@sit.shonan-it.ac.jp>
2025-06-03 12:12:12 +0900

    Create Inst.java

---
+++ project/Inst.java
@@ -0,0 +1,8 @@
+public class Inst 
+{ 
+    public static void main(String[] args) 
+    { 
+        StringBuffer sb = new StringBuffer( "勝利も敗北も知り逃げ回って涙を流して男は一人前になる！\n" ); 
+        System.out.println(sb.toString() + "泣いたっていいんだ！");
+    } 
+}
